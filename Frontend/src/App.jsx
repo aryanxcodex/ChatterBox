a73bc9599a7b05520681984bd38c3034aeb260b6
2003aryansingh <s424.aryansingh@gmail.com>
2024-09-07 13:00:37 +0530

    configured shadcn

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from './components/ui/button';
+import "./App.css";
+
+function App() {
+  return (
+    <Button>Hello</Button>
+  );
+}
+
+
+export default App;
